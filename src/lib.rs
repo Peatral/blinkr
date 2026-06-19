b2a28223f508d7fba26a6dea955db732238db714
Christof Reimers <contact@peatral.xyz>
2026-06-19 22:05:04 +0200

    Decrease inbox/outbox size to save heap

--- src/lib.rs
+++ src/lib.rs
@@ -34,12 +34,7 @@ pub fn main() -> isize {
     AppMessage::register_inbox_received(ui::settings_window::inbox_received_handler);
     AppMessage::register_outbox_sent(sync::outbox_sent_handler);
     AppMessage::register_outbox_failed(sync::outbox_failed_handler);
-    if AppMessage::open(
-        AppMessage::inbox_size_maximum(),
-        AppMessage::outbox_size_maximum(),
-    )
-    .is_err()
-    {
+    if AppMessage::open(256, 512).is_err() {
         pebble::pbl_err!(c"Failed to open AppMessage subsystem!");
     }
 
