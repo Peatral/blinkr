6210752805432b84d8c2a15f876d2e0e8e724adc
Christof Reimers <contact@peatral.xyz>
2026-09-04 15:06:12 +0200

    Fix watchapp only syncing once

--- pebble/src/sync.rs
+++ pebble/src/sync.rs
@@ -1,15 +1,7 @@
 use crate::message_queue::{push_message, Message};
 use crate::state;
-use pebble::types::GlobalCell;
-
-static SYNC_IN_PROGRESS: GlobalCell<bool> = GlobalCell::new(false);
-static CURRENT_CHUNK_INDEX: GlobalCell<usize> = GlobalCell::new(0);
 
 pub fn start_sync() {
-    if SYNC_IN_PROGRESS.get() {
-        return;
-    }
-
     let history = state::HISTORY.borrow();
     if history.is_empty() {
         return;
@@ -17,9 +9,6 @@ pub fn start_sync() {
 
     let chunks = history.chunks(state::PAIRS_PER_CHUNK);
     let total_chunks = chunks.len() as i32;
-
-    SYNC_IN_PROGRESS.set(true);
-    CURRENT_CHUNK_INDEX.set(0);
 
     push_message(Message::SyncStart { total_chunks });
 
