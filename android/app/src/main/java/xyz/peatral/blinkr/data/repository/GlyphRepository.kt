ad12317fcd00e5bb32747e8c0ee98eaa11a83122
Christof Reimers <contact@peatral.xyz>
2026-09-10 15:37:57 +0200

    Improve glyph matrix cleanup sequence

--- android/app/src/main/java/xyz/peatral/blinkr/data/repository/GlyphRepository.kt
+++ android/app/src/main/java/xyz/peatral/blinkr/data/repository/GlyphRepository.kt
@@ -1,6 +1,11 @@
 package xyz.peatral.blinkr.data.repository
 
 import xyz.peatral.blinkr.data.datasource.GlyphDataSource
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -9,6 +14,7 @@ class GlyphRepository @Inject constructor(
     private val glyphDataSource: GlyphDataSource
 ) {
     var references = 0
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
 
     fun connect() {
         glyphDataSource.connect()
@@ -18,11 +24,21 @@ class GlyphRepository @Inject constructor(
     fun disconnect() {
         references = 0.coerceAtLeast(references - 1)
         if (references <= 0) {
-            glyphDataSource.disconnect()
+            clearDisplay()
+            scope.launch {
+                delay(100)
+                if (references <= 0) {
+                    glyphDataSource.disconnect()
+                }
+            }
         }
     }
 
     fun displayText(text: String, x: Int, y: Int) {
         glyphDataSource.displayText(text, x, y)
+    }
+
+    fun clearDisplay() {
+        glyphDataSource.clearDisplay()
     }
 }
